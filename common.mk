bad1bcf087f0b2ac098a770877ad70afb08fd228
LuK1337 <priv.luk@gmail.com>
2026-01-25 19:47:35 +0100

    sm8850-common: Add xbl_config_arb_check postinstall for /odm
    Change-Id: I9e2f6e1063aa6414790a11b08321df4b0fe5b13b
    Signed-off-by: Ahtesham Malik <ehteshammalik18998@gmail.com>

--- common.mk
+++ common.mk
@@ -49,9 +49,16 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_odm=true \
+    POSTINSTALL_PATH_odm=bin/xbl_config_arb_check \
+    FILESYSTEM_TYPE_odm=erofs \
+    POSTINSTALL_OPTIONAL_odm=false
+
 PRODUCT_PACKAGES += \
     checkpoint_gc \
-    otapreopt_script
+    otapreopt_script \
+    xbl_config_arb_check
 
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
