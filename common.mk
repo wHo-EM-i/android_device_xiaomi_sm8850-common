afbd4be448f19fa9e51aabd23cdc964a4eac2cbb
Arian <arian.kulmer@web.de>
2026-03-23 14:51:36 +0000

    sm8850-common: Generate qcrilNr.db during build
    Change-Id: I1f3425713b9b5b1cc7185bc94124b9f01efe3abb

--- common.mk
+++ common.mk
@@ -312,6 +312,7 @@ PRODUCT_PACKAGES += \
     extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
+    qcrilNrDb_vendor \
     qti-telephony-hidl-wrapper \
     qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper.xml \
