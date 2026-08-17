a73236727f8c9aa29b72548289a259ffd1112f86
chandu078 <chandudyavanapelli03@gmail.com>
2026-08-17 19:04:06 +0000

    sm8850-common: Switch to foreground to use the big cluster
    Change-Id: I49977192f54605b7e50331aaa9ef307df8afbe2e
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- common.mk
+++ common.mk
@@ -263,7 +263,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/mdpf.json:$(TARGET_COPY_OUT_VENDOR)/etc/mdpf.json
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/canoe/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
