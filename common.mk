7ff104f32f3b7c968ff21ea4e67aa804e0140453
wHo-EM-i <ehteshammalik18998@gmail.com>
2026-08-27 02:55:15 +0530

    sm8850-common: Build LunarisDolby
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- common.mk
+++ common.mk
@@ -141,6 +141,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    LunarisDolby
+
+PRODUCT_COPY_FILES += \
+    hardware/dolby/configs/dax/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
